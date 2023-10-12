211b4c7e5c9935fca8a9722d5e733f88dde9ed56
princetomar <madm47157@gmail.com>
2023-10-13 02:37:20 +0530

    Offers Section Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
+import Offers from "./components/Offers";
 import { BrowserRouter as Router } from "react-router-dom";
 import data from "./data/data.json";
 
@@ -11,6 +12,7 @@ function App() {
       <PreNavbar />
       <Navbar />
       <Slider start={data.banner.start} />
+      <Offers offers={data.offer} />
     </Router>
   );
 }
---
+++ src/components/Offer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Offer = ({ src, link, index }) => {
+  return (
+    <a href={link}>
+      <img src={src} alt={`${index} Offer`} />
+    </a>
+  );
+};
+export default Offer;
